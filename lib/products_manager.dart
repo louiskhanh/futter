c9e3ae8f48f23bd0d1bbcced6784e16f9ab47cbf
louiskhanh <louiskhanh@gmail.com>
2021-11-25 08:11:26 +0900

    First commit

---
+++ lib/products_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import  './products.dart';
+
+class ProductManager extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState(){
+    return _ProductManagerState();
+  }
+
+}
